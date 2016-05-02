fa819e2e0387b8b151d1f97ad00c82c676f093c0
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-05-02 21:07:15 +0300

    Regression test done

--- src/test/groovy/org/ka/test/integration/TradeServiceRegressionTest.groovy
+++ src/test/groovy/org/ka/test/integration/TradeServiceRegressionTest.groovy
@@ -3,17 +3,19 @@ package org.ka.test.integration
 import groovy.sql.Sql
 import org.ka.trades.dao.TradeDao
 import org.ka.trades.dao.TradeProcessingDao
+import org.ka.trades.model.Trade
 import org.ka.trades.parser.PipeSeparatedValuesParser
 import org.ka.trades.service.ParseAndSaveTradesToDatabase
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class TradeServiceRegressionTest extends Specification {
 
     static Sql sql
 
-    def 'do regression test'() {
+    def 'do regression test for successful messages'() {
         given:
         def parser = new PipeSeparatedValuesParser()
         def tradeDao = Mock(TradeDao)
@@ -22,9 +24,25 @@ class TradeServiceRegressionTest extends Specification {
         when:
         service.process(message)
         then:
-        noExceptionThrown()
+        1 * tradeDao.save(_)
+        and:
+        1 * tradeProcessingDao.success(message)
         where:
-        message << sql.rows("select * from TRADES_PROCESSING").collect { it['TRADE_INFO'] }
+        [message, result, errorMessage] << sql.rows("select trade_info, result, error_message from TRADES_PROCESSING where result = 'SUCCESS'")
+    }
+
+    def 'do regression test for failed messages'() {
+        given:
+        def parser = new PipeSeparatedValuesParser()
+        def tradeDao = Mock(TradeDao)
+        def tradeProcessingDao = Mock(TradeProcessingDao)
+        def service = new ParseAndSaveTradesToDatabase(parser, tradeDao, tradeProcessingDao)
+        when:
+        service.process(message)
+        then:
+        1 * tradeProcessingDao.failure(message, errorMessage)
+        where:
+        [message, result, errorMessage] << sql.rows("select trade_info, result, error_message from TRADES_PROCESSING where result = 'FAILURE'")
     }
 
     def setupSpec() {
